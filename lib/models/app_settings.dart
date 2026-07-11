73b7856c254cfa25a85afd83cd775149ba955972
ibrashov <ibrashovanuar@gmail.com>
2026-07-11 10:33:15 +0500

    added time interval 1,2,3 hours with self change

--- lib/models/app_settings.dart
+++ lib/models/app_settings.dart
@@ -1,67 +1,33 @@
 import 'app_language.dart';
 import 'notification_length.dart';
-import 'notification_time.dart';
 
 class AppSettings {
-  const AppSettings({
-    required this.language,
-    required this.length,
-    required this.notificationTimes,
-  });
+  const AppSettings({required this.language, required this.length});
 
   final AppLanguage language;
   final NotificationLength length;
-  final List<NotificationTime> notificationTimes;
 
   static const defaultSettings = AppSettings(
     language: AppLanguage.ru,
     length: NotificationLength.medium,
-    notificationTimes: <NotificationTime>[
-      NotificationTime(hour: 9, minute: 0),
-    ],
   );
 
   factory AppSettings.fromJson(Map<String, dynamic> json) {
-    final rawTimes = json['notificationTimes'];
-    final times = rawTimes is List
-        ? rawTimes
-              .whereType<Map>()
-              .map((item) => NotificationTime.fromJson(
-                    Map<String, dynamic>.from(item),
-                  ))
-              .toList()
-        : <NotificationTime>[];
-    times.sort();
-
     return AppSettings(
       language: AppLanguage.fromCode(json['language'] as String?),
       length: NotificationLength.fromId(json['lengthMode'] as String?),
-      notificationTimes:
-          times.isEmpty ? defaultSettings.notificationTimes : times,
     );
   }
 
   Map<String, dynamic> toJson() => <String, dynamic>{
     'language': language.code,
     'lengthMode': length.id,
-    'notificationTimes': notificationTimes
-        .map((time) => time.toJson())
-        .toList(),
   };
 
-  AppSettings copyWith({
-    AppLanguage? language,
-    NotificationLength? length,
-    List<NotificationTime>? notificationTimes,
-  }) {
-    final nextTimes = List<NotificationTime>.from(
-      notificationTimes ?? this.notificationTimes,
-    )..sort();
-
+  AppSettings copyWith({AppLanguage? language, NotificationLength? length}) {
     return AppSettings(
       language: language ?? this.language,
       length: length ?? this.length,
-      notificationTimes: nextTimes,
     );
   }
 }
