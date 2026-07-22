c3727c2e01dd685db06ebdbf8de102a307ff11b7
ibrashov <ibrashovanuar@gmail.com>
2026-07-23 00:08:53 +0500

    Fully added working time intervals

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:unebil/main.dart';
 import 'package:unebil/models/notification_interval.dart';
+import 'package:unebil/screens/settings_screen.dart';
 
 import 'app_controller_test.dart';
 
@@ -43,6 +44,42 @@ void main() {
 
     expect(controller.settings.language.label, 'Қазақша');
     expect(controller.settings.length.targetWords, 70);
+  });
+
+  test('adds hours to a time and wraps after midnight', () {
+    expect(
+      addHoursToTimeOfDay(const TimeOfDay(hour: 22, minute: 45), 3),
+      const TimeOfDay(hour: 1, minute: 45),
+    );
+  });
+
+  testWidgets('chooses an interval before opening the time picker', (
+    tester,
+  ) async {
+    final controller = await createController();
+
+    await tester.pumpWidget(UneBilApp(controller: controller));
+    await tester.tap(find.byIcon(Icons.tune));
+    await tester.pumpAndSettle();
+    await tester.scrollUntilVisible(
+      find.text('Добавить время'),
+      200,
+      scrollable: find.byType(Scrollable).first,
+    );
+    await tester.tap(find.text('Добавить время'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Через сколько часов?'), findsOneWidget);
+    expect(find.text('+1 ч'), findsOneWidget);
+    expect(find.text('+2 ч'), findsOneWidget);
+    expect(find.text('+3 ч'), findsOneWidget);
+
+    await tester.tap(find.text('+2 ч'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Выбери время'), findsOneWidget);
+    expect(find.text('Отмена'), findsOneWidget);
+    expect(find.text('Готово'), findsOneWidget);
   });
 
   testWidgets('changes interval on topic screen and shows fact schedule', (
