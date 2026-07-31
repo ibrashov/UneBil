ae54c055b0d4e1027c548ba4c62cfba8cc1b8323
ibrashov <ibrashovanuar@gmail.com>
2026-07-31 23:12:26 +0500

    chandeg to device time so doenst need to change time zones for every country

--- lib/models/app_time_zone.dart
+++ lib/models/app_time_zone.dart
@@ -1,4 +1,5 @@
 enum AppTimeZone {
+  device('device', 'Device time', null),
   kazakhstan('kazakhstan', 'Kazakhstan', 'Asia/Almaty'),
   china('china', 'China', 'Asia/Shanghai'),
   spain('spain', 'Spain', 'Europe/Madrid');
@@ -9,12 +10,13 @@ enum AppTimeZone {
   final String label;
 
   /// IANA timezone name used by the timezone package.
-  final String locationName;
+  /// `null` means that the IANA zone reported by the device is used.
+  final String? locationName;
 
   static AppTimeZone fromId(String? id) {
     return AppTimeZone.values.firstWhere(
       (timeZone) => timeZone.id == id,
-      orElse: () => AppTimeZone.kazakhstan,
+      orElse: () => AppTimeZone.device,
     );
   }
 }
